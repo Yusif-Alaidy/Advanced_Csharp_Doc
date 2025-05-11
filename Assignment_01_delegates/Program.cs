37f79d293cb1dea6f5fa9be3378f6652530501e6
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-05-12 00:41:50 +0300

    (Delegates):add assignment and lesson

---
+++ Assignment_01_delegates/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Assignment_01_delegates
+{
+    internal class Program
+    {
+        // Interface
+        public interface IPaymentMethod
+        {
+            void Pay(decimal amount);
+        }
+
+        public class CreaditCardPayment : IPaymentMethod
+        {
+            public void Pay(decimal amount)
+            {
+                Console.WriteLine($"[Credit Card] and amount is {amount}.");
+            }
+        }
+        
+        public class PaypalPayment : IPaymentMethod
+        {
+            public void Pay(decimal amount)
+            {
+                Console.WriteLine($"[Paypal] and amount is {amount}.");
+            }
+        }
+              
+        public class CashPayment : IPaymentMethod
+        {
+            public void Pay(decimal amount)
+            {
+                Console.WriteLine($"[Cash] and amount is {amount}.");
+            }
+        }
+        
+        public class PaymentProcessor
+        {
+            private readonly Dictionary<string, IPaymentMethod> _paymentMethods;
+            public PaymentProcessor()
+            {
+                _paymentMethods = new Dictionary<string, IPaymentMethod>();
+            }
+            public void RegisterPaymentMethod(string methodName, IPaymentMethod method)
+            {
+                _paymentMethods[methodName] = method;
+            }
+            public void ProcessPayment(string methodname, decimal amount)
+            {
+                if (_paymentMethods.ContainsKey(methodname))
+                {
+                    Console.WriteLine("Loading...");
+                    _paymentMethods[methodname].Pay(amount);
+                    Console.WriteLine("Done ");
+                }
+                else
+                {
+                    Console.WriteLine("payment method is not avaliable.");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var processor = new PaymentProcessor();
+            processor.RegisterPaymentMethod("paypal", new PaypalPayment());
+
+            decimal orderAmount = 450.00m;
+            string selectedMethod = "paypal";
+
+            processor.ProcessPayment(selectedMethod, orderAmount);
+
+        }
+    }
+}
---
+++ Lesson_01_delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lesson_01_delegates
+{
+    // Delegates Documentation in Microsoft
+    // https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/delegates/
+    internal class Program
+    {
+        // Single-cast Delegate =============================╗
+        delegate void PrintMessage();                      //║
+        static void Hello() => Console.WriteLine("Hello"); //║
+        // ==================================================╝
+        
+        // Multi-cast Delegate ===================================╗
+        delegate void Notify();                                // ║
+        static void Msg1() => Console.WriteLine("Message 1");  // ║
+        static void Msg2() => Console.WriteLine("Message 2");  // ║
+        // =======================================================╝
+
+        static void Main(string[] args)  
+        {
+            // Single-cast Delegate ======╗
+            PrintMessage msg = Hello;  // ║
+            msg();  // يطبع Hello      // ║
+            // ===========================╝
+
+            // Multi-cast Delegate ========╗
+            Notify notify = Msg1;       // ║
+            notify += Msg2;             // ║
+            notify();                   // ║
+            // ============================╝
+
+
+        }
+    }
+}
