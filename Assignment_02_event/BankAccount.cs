65cd9af2a7f9a08cb0e0b11e42d630cb77ea0a42
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-05-14 02:48:13 +0300

    Creat: event parctice

---
+++ Assignment_02_event/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_02_event
+{
+    class BankAccount
+    {
+        public event amoutIsLowEvenHandler amountIsLow;
+        public delegate void amoutIsLowEvenHandler();
+
+        float amount { get; set; }
+
+        public void withdraw(float money)
+        {
+            amount -= money;
+            if (amount < 30)
+            {
+                amountIsLow?.Invoke();
+            } 
+            return;
+        }        
+        public void Deposit(float money)
+        {
+            amount += money;
+            return;
+        }
+
+        public void myCash()
+        {
+            Console.WriteLine($"your cash is: {amount}");
+        }
+
+        
+
+    }
+}
---
+++ Assignment_02_event/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment_02_event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount yusif = new BankAccount();
+            yusif.amountIsLow += ()=>Console.WriteLine("your money is under the 30 pound...");
+
+            yusif.Deposit(100);
+            yusif.withdraw(80);
+            yusif.myCash();
+        }
+    }
+}
