2aa40e641ec9fc2718aaf36f9c5fbd137c5ecec3
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-05-19 19:05:56 +0300

    Build: create Enumerables

---
+++ Enumerables/Lesson_03_Enumerables/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_03_Enumerables
+{
+    public class Employee
+    {
+        private readonly List<string> names = new();
+        public void AddName(string name)
+        {
+            names.Add(name);
+            return;
+        }
+
+    public IEnumerator<string> GetEnumerator()
+        {
+            return names.GetEnumerator();
+        }
+    }
+}
---
+++ Enumerables/Lesson_03_Enumerables/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lesson_03_Enumerables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new();
+
+            employee1.AddName("yusif");
+            employee1.AddName("ali");
+            employee1.AddName("mohsen");
+            employee1.AddName("mena");
+
+            foreach (var emp in employee1)
+            {
+                Console.WriteLine($"name is: {emp}");
+            }
+        }
+    }
+}
