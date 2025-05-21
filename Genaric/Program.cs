d49f3eb11c07ac11053a97c7d4d1c8d9cf7a34b2
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-05-21 20:29:31 +0300

    Build: genaric lesson

---
+++ Genaric/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlTypes;
+using System.Numerics;
+
+namespace Genaric
+{
+    internal class Program
+    {
+        public static T add<T>(T a,T b) where T : INumber<T>
+        {
+
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(add(5.5,6.1));
+        }
+    }
+}
