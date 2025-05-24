61f6529be9fcb96de5abd46850f8b93e394a6ee7
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-05-25 00:37:25 +0300

    Build: Create Linq Lesson

---
+++ Lesson_Linq/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lesson_Linq
+{
+    internal class Program
+    {
+        // Linq is Deferred Execution
+        static void Main(string[] args)
+        {
+            List<int> myList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            // Query expression basics (Query syntax)
+            var Query = from item in myList where item > 5 select item; // output: 6, 7, 8, 9, 10
+
+            // Method syntax 
+            var QuaryMethod = myList.Where(x => x > 5); // output: 6, 7, 8, 9, 10
+
+            // Sorting: OrderBy, OrderByDescending, ThenBy, ThenByDescendingm reverse
+            // https://learn.microsoft.com/en-us/dotnet/csharp/linq/standard-query-operators/sorting-data
+
+            // Quantifiers: all, any, contains
+            // https://learn.microsoft.com/en-us/dotnet/csharp/linq/standard-query-operators/quantifier-operations
+
+            // partitioning: Take, TakeWhile, Skip, SkipWhile, chunk
+            // https://learn.microsoft.com/en-us/dotnet/csharp/linq/standard-query-operators/partitioning-data
+
+            // Set Operations: distinct, union, intersect, except, concat 
+            // https://learn.microsoft.com/en-us/dotnet/csharp/linq/standard-query-operators/set-operations
+
+        }
+    }
+}
